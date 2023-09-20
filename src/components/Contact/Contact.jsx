f46c5b5b826541068b49dd5088d09dd9eec11b5e
jamessuryawijaya59@gmail.com <jamessuryawijaya59@gmail.com>
2023-09-20 21:09:55 +1000

    animation text on contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,15 @@
 import "./contact.css";
-import { useRef } from "react";
+import { useRef,useEffect } from "react";
 import emailjs from "@emailjs/browser";
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Model from "./Model"; /* highlight-line */
+import gsap from "gsap";
+
+
+
+
 
 function Contact() {
   const modelSrc = "https://models.readyplayer.me/64c7b1af067a35dfd8b3de7f.glb";
@@ -35,14 +40,45 @@ function Contact() {
     e.target.reset();
   };
 
+
+  const revealElements = useRef([]);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('reveal__content');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    revealElements.current.forEach(element => {
+      observer.observe(element);
+    });
+  }, []);
+
+
   return (
     <section id="contact-section">
       <span className="heading-project">
         {" "}
-        <h2>
+        <div className="singleLine">
+        <div className="reveal" ref={el => revealElements.current.push(el)}>
+        <div className="reveal__content"> <h2>
           LET'S CONNECT & GET TO KNOW<br></br> EACH OTHER
         </h2>
+        </div>
+        </div>
+       </div>
       </span>
+
       <div className="container">
         <div className="container-1">
           <div className="container-abstracts">
@@ -53,6 +89,8 @@ function Contact() {
               FEEL FREE TO REACH OUT ILL CONTACT<br></br>AS SOON AS I CAN THANK
               YOU
             </p>
+
+
             <div className="ball" id="ball-1"></div>
             <div className="abstracts"></div>
             <div className="ball"></div>
