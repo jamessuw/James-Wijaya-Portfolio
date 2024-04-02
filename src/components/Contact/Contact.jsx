36a87667d490d50bc581e887348ebf8f8700b34e
jamessuryawijaya59@gmail.com <jamessuryawijaya59@gmail.com>
2024-04-02 19:26:05 +1100

    fixing scrolling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -117,7 +117,7 @@ function Contact() {
                 Send massages
               </button>
             </form>
-           <ScrollLink to="top" smooth={true} >
+           <ScrollLink to="top" smooth={true} spy={true} >
         <button className="scroll-top-btn">
           Scroll to Top
         </button>
